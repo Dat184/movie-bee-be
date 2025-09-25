fae6a96ede23208212628b285b887e259d9ecf14
thdat <thanhnguyendat184@gmail.com>
2025-09-25 14:46:31 +0700

    add class-validator. Config and use for validate in DTO

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public')); // serve static files from the "public" directory like CSS, JS, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // set the directory for view templates
   app.setViewEngine('ejs');
+
+  // enable validation globally
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email should not be empty' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password should not be empty' })
+  @Min(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Name should not be empty' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Age should not be empty' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Address should not be empty' })
+  address: string;
+}
