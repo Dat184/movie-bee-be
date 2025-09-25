6cf93e91ed5ff4d3b93e00b13b5b1c2fd70c0806
thdat <thanhnguyendat184@gmail.com>
2025-09-25 14:29:25 +0700

    add @nestjs/config for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    // ConfigModule.forRoot() để tìm kiếm file env nào sử dụng và validate các biến môi trường trong file env đó
+    ConfigModule.forRoot({
+      isGlobal: true, // để có thể sử dụng ConfigModule ở bất kỳ module nào mà không cần import lại
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
-  // config view engine ejs to render our HTML views
+  // config view engine ejs to render our HTML views (Server-Side Rendering)
   app.useStaticAssets(join(__dirname, '..', 'public')); // serve static files from the "public" directory like CSS, JS, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // set the directory for view templates
   app.setViewEngine('ejs');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
 
